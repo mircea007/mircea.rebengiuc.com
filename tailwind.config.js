b5baedaf3f1ffe114a340a992c795e6e14d72f57
mircea_007 <mirceareb@gmail.com>
2021-10-15 00:00:50 +0300

    expand projects, and put font in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ["Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji"]
+    }
   },
   variants: {
     extend: {
