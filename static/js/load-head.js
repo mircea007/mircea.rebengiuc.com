dbb2673c371fd83e2c544caeb6498ac3daf9dac0
mircea_007 <mirceareb@gmail.com>
2021-10-15 09:29:48 +0300

    add icon, common meta template and make current page highlighted in navbar

--- static/js/load-head.js
+++ static/js/load-head.js
@@ -1,4 +1,5 @@
 var root_prefix = "/";
 $("#header").load(root_prefix + "templates/header.html");
 $("#footer").load(root_prefix + "templates/footer.html");
+$("#meta"  ).load(root_prefix + "templates/meta.html"  );
 window.my_custom_templates_loaded = true;
